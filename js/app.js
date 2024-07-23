7315bb3733b272973ff33f0cd2f9639e6a4aa018
Denwebart <denwebart@gmail.com>
2024-07-23 16:51:14 +0300

    new feature(.main-menu sticky): Main navigation now sticky when you scroll on it

--- js/app.js
+++ js/app.js
@@ -82,3 +82,9 @@ media.addEventListener('change', function (e) {
 	setupTopNav(e);
 });
 // Burger Menu END
+
+// Detecting When a Sticky Element Gets Pinned
+const el = document.querySelector('.main-menu');
+const observer = new IntersectionObserver(([e]) => e.target.classList.toggle('is-pinned', e.intersectionRatio < 1), { threshold: [1] });
+
+observer.observe(el);
