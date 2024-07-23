7373c08d2f03b710d7c9e75b8b8b3760133ab208
Denwebart <denwebart@gmail.com>
2024-07-23 15:22:06 +0300

    new feature(.scrollBtn): Scroll to top button

--- js/app.js
+++ js/app.js
@@ -1,10 +1,43 @@
-// Burger Menu
+/* Scroll to Top button START */
+
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+		document.getElementById('scrollBtn').style.display = 'flex';
+		document.getElementById('scrollBtn').style.alignItems = 'center';
+		document.getElementById('scrollBtn').style.justifyContent = 'center';
+	} else {
+		document.getElementById('scrollBtn').style.display = 'none';
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document smoothly
+function scrollToTop() {
+	if (window.scrollTo) {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth', // Smooth scrolling
+		});
+	} else {
+		// Old browsers support
+		document.body.scrollTop = 0; // For Safari
+		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE и Opera
+	}
+}
+
+/* Scroll to Top button END */
+
+// Burger Menu START
 const btnOpen = document.querySelector('#btnOpen');
 const btnClose = document.querySelector('#btnClose');
 const media = window.matchMedia('(width < 992px)');
 const topNavMenu = document.querySelector('.main-menu-wrapper');
 const main = document.querySelector('main');
 const body = document.querySelector('body');
+const scrollBtn = document.getElementById('scrollBtn');
 
 function setupTopNav(e) {
 	if (e.matches) {
@@ -23,6 +56,7 @@ function openMobileMenu() {
 	topNavMenu.removeAttribute('style');
 	main.setAttribute('inert', ' ');
 	body.style.overflow = 'hidden';
+	scrollBtn.style.display = 'none';
 	btnClose.focus();
 }
 
@@ -31,6 +65,7 @@ function closeMobileMenu() {
 	topNavMenu.setAttribute('inert', ' ');
 	main.removeAttribute('inert');
 	body.style.overflow = 'visible';
+	scrollBtn.style.display = 'flex';
 	btnOpen.focus();
 
 	setTimeout(() => {
@@ -46,3 +81,4 @@ btnClose.addEventListener('click', closeMobileMenu);
 media.addEventListener('change', function (e) {
 	setupTopNav(e);
 });
+// Burger Menu END
